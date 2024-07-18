a1bba8708c5932cb943fc474e182287054c0e70a
AdelMysterio <storliniadelmo@gmail.com>
2024-07-18 13:42:57 +0200

    inseriti i dati in pagina attraverso model e controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.app')
 
 @section('main-content')
-
-<main>
-
-</main>
-
+    <main>
+        <h1>Elenco dei Treni</h1>
+        @foreach ($trains as $train)
+            <article>
+                <h2>{{ $train->Azienda }}</h2>
+                <p>Stazione di partenza: {{ $train['Stazione di partenza'] }}</p>
+                <p>Stazione di arrivo: {{ $train['Stazione di arrivo'] }}</p>
+                <p>Orario di partenza: {{ $train['Orario di partenza'] }}</p>
+                <p>Orario di arrivo: {{ $train['Orario di arrivo'] }}</p>
+                <p>Codice Treno: {{ $train['Codice Treno'] }}</p>
+                <p>Numero Carrozze: {{ $train['Numero Carrozze'] }}</p>
+                <p>In orario: {{ $train['In orario'] ? 'Sì' : 'No' }}</p>
+                <p>Cancellato: {{ $train['Cancellato'] ? 'Sì' : 'No' }}</p>
+            </article>
+        @endforeach
+    </main>
 @endsection
